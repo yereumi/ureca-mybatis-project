9cd71c3f15673035b8885d3629389dd19d92274a
yereumi <lovelyyeji01@gmail.com>
2025-03-12 11:38:09 +0900

    Initial commit

---
+++ src/main/java/kr/co/ureca/mybatis/UrecaMybatisProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ureca.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaMybatisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecaMybatisProjectApplication.class, args);
+	}
+
+}
